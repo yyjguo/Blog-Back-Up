32f936e2bf8033fdc3f1c8603bbdbff4c991996a
zhengjingfa <643324224@qq.com>
2017-05-08 15:52:46 +0800

    add photos

--- tool.py
+++ tool.py
@@ -129,7 +129,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("../zhengjingfa.github.io/source/photos/data.json","w") as fp:
+    with open("Users/afanail/source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
